8e3f1d7c2e79215b6c51ee03fb2bd93a2bfddf02
david <davidombu@gmail.com>
2023-11-29 06:31:03 -0500

    Improvement code

--- app/src/main/java/pe/com/test/data/network/dto/MovieUpcomingBase.kt
+++ app/src/main/java/pe/com/test/data/network/dto/MovieUpcomingBase.kt
@@ -1,7 +1,6 @@
 package pe.com.test.data.network.dto
 
 import com.google.gson.annotations.SerializedName
-import pe.com.test.data.network.dto.MovieUpcoming
 
 class MovieUpcomingBase(
     val page: Int,
